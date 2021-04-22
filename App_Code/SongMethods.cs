95a228293c760d570b41ddf0fc4cf2c1cfd8b1d8
Lucas Joshua <lucasjoshua04@hotmail.com>
2021-04-22 11:27:41 +0200

    Form add song gecodeerd

--- App_Code/SongMethods.cs
+++ App_Code/SongMethods.cs
@@ -9,40 +9,50 @@ namespace playlistApp.App_Code
 {
     public class SongMethods
     {
-        //functies en variabelen.
-        public DataSet GetAllSongs()
+        DataSet ds = new DataSet("playlist");
+
+                                                                                                     //functies en variabelen.
+        public DataSet GetAllSongs(string filePath)
         {
-            //structuur die de database in memory kan bevatten.
-
-            DataSet ds = new DataSet("playlist");
-
+                                                                                                    //structuur die de database in memory kan bevatten.            
             DataTable song = new DataTable("song");
-
             DataColumn artist = new DataColumn("artist");
             DataColumn title = new DataColumn("title");
             DataColumn year = new DataColumn("year");
             DataColumn album = new DataColumn("album");
 
 
-            //kolommen in detabel zetten. 
+                                                                                                     //kolommen in detabel zetten. 
 
             song.Columns.Add(artist);
             song.Columns.Add(title);
             song.Columns.Add(album);
             song.Columns.Add(year);
 
-            //tabel in de db zetten 
+                                                                                                     //tabel in de db zetten 
             ds.Tables.Add(song);
 
 
 
-            //hier moeten we het bestand playlist.xml inlezen....
-            //????
+                                                                                                      //hier moeten we het bestand playlist.xml inlezen....
 
 
-            ds.ReadXml(HttpContext.Current.Server.MapPath("~/playlist.xml"));
+            ds.ReadXml(HttpContext.Current.Server.MapPath(filePath));
             return ds;
         }
+
+        public DataRow GetEmptyDataRow()
+        {
+            DataRow dr = ds.Tables["song"].NewRow();
+            return dr;
+        }
+
+        public void CreateSong(DataRow dataRow, string file)
+        {
+            ds.Tables["song"].Rows.Add(dataRow);
+            ds.WriteXml(HttpContext.Current.Server.MapPath(file));
+        }
+
     }
     // crud acties komen hieronder. 
 }
